f641a9e3ab59cfb3119995b3f1750dbdc822fde6
loikobi <loint.24ite@vku.udn.vn>
2025-09-25 09:58:02 +0700

    db lab5

--- php_basic/init.sql
+++ php_basic/init.sql
@@ -1,10 +1,7 @@
--- Tạo database nếu chưa tồn tại và sử dụng bộ ký tự utf8mb4 để hỗ trợ tiếng Việt tốt nhất
 CREATE DATABASE IF NOT EXISTS `lab5` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Chuyển sang sử dụng database vừa tạo
 USE `lab5`;
 
--- Tạo bảng `products`
 CREATE TABLE IF NOT EXISTS `products` (
   `productID` INT AUTO_INCREMENT PRIMARY KEY,
   `productCode` VARCHAR(255) NOT NULL UNIQUE,
